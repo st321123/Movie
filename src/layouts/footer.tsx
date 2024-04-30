645b83f45d278982cffae31a65c12c4924e19893
Shubham Tewari <st530535@gmail.com>
2024-04-30 20:16:03 +0530

    First

---
+++ src/layouts/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <div className="p-6 text-center">
+      <p className="text-sm opacity-[0.7]">
+        &#169; 2023 Shubham Tewari. All Right Reserved.
+      </p>
+    </div>
+  )
+}
